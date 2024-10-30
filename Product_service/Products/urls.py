16c6c6192d73573a7398ed04c6ef249e10477782
WilsonLiu2002 <Liujiacheng020509@163.com>
2024-10-30 14:34:41 -0400

    router

--- Product_service/Products/urls.py
+++ Product_service/Products/urls.py
@@ -1,6 +1,9 @@
 # urls.py
-from django.urls import path
+from django.urls import path, include
 from .views import *
+
+router = DefaultRouter()
+router.register(r'foods', FoodViewSet, basename='food')
 
 urlpatterns = [
     path('add/', add_food_item, name='add_food_item'),
@@ -8,4 +11,5 @@ urlpatterns = [
     path('delete/<str:restaurant>/<str:name>/', delete_food, name='delete_food'),
     path('everything', get_all_food_items, name='get_all_food_items'),
     path('getMenu/<str:restaurant>/', get_food_items_by_restaurant, name='get_food_items_by_restaurant'),
+    path('', include(router.urls)),
 ]
--- Product_service/Products/views.py
+++ Product_service/Products/views.py
@@ -4,6 +4,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Food
 from .serializers import FoodSerializer
+from rest_framework import viewsets
+from rest_framework.routers import DefaultRouter
+
+class FoodViewSet(viewsets.ModelViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+router = DefaultRouter()
+router.register(r'foods', FoodViewSet, basename='food')
+
 
 @api_view(['GET'])
 def base_message(request):
