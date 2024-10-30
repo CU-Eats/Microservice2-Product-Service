80d30ef2366aff47fa87e4c88683ba8e9b524b0b
WilsonLiu2002 <Liujiacheng020509@163.com>
2024-10-30 13:48:44 -0400

    delete service

--- project/meals/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import foods
-# Register your models here.
-
-admin.site.register(foods)
--- project/meals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import foods
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = foods
-        fields = '__all__'
