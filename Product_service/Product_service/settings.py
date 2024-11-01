37b2fcfa21483290560da2d1703322f51a135846
dshen07 <dshen@ucsd.edu>
2024-11-01 15:39:29 -0400

    change settings rds connection

--- Product_service/Product_service/settings.py
+++ Product_service/Product_service/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,11 +84,11 @@ WSGI_APPLICATION = "Product_service.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Products',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'database-2.c30iyquoyimx.us-east-2.rds.amazonaws.com',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
